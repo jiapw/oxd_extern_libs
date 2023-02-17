0c4e5b740c97c6a6f6e89f9979a9f56c49751d32
ZHANG XIAOBING <zhangxbng@gmail.com>
2023-02-17 11:03:20 +0800

    !R: simple tools

---
+++ include/codec/tools.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include<string>
+#include "cppcodec/base64_rfc4648.hpp"
+#include "cppcodec/base64_url.hpp"
+
+namespace codec
+{
+
+class Base64
+{
+public:
+    static std::string Encode(std::string_view s)
+    {
+        return cppcodec::base64_rfc4648::encode(s.data(), s.size());
+    }
+    static std::vector<uint8_t> Decode(std::string_view s)
+    {
+        return cppcodec::base64_rfc4648::decode(s.data(), s.size());
+    }
+    static std::string EncodeUrl(std::string_view s)
+    {
+        return cppcodec::base64_url::encode(s.data(), s.size());
+    }
+};
+
+}
