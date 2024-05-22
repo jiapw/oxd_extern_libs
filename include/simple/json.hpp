0050acbea48d47081e399c206c37332cabc6c5a1
ZHANG XIAOBING <zhangxbng@gmail.com>
2024-05-22 21:39:04 +0800

    !A: support more value types

--- include/simple/json.hpp
+++ include/simple/json.hpp
@@ -14,6 +14,11 @@ namespace simple {
 		json() {};
 		json(std::string_view input) { if (!input.empty()) parse(input); }
 
+		json& operator=(const nlohmann::json& other)
+		{
+			*(nlohmann::json*)this = other;
+			return *this;
+		}
 		bool parse(std::string_view input)
 		{
 			*(nlohmann::json*)this = nlohmann::json::parse(input, nullptr, false);
@@ -23,16 +28,51 @@ namespace simple {
 		{
 			return !is_discarded();
 		}
-		bool get_kv_string(std::string_view key, std::string& out)
+		template<typename T>
+		bool get_kv(std::string_view key, T& out)
 		{
 			if (!contains(key))
 				return false;
 
 			auto& v = this->operator[](key);
-			if (!v.is_string())
-				return false;
+
+			if constexpr(std::is_same_v<T, std::string>)
+			{
+				if (!v.is_string())
+					return false;
+			}
+			else if constexpr (std::is_same_v<T, bool>)
+			{
+				if (!v.is_boolean())
+					return false;
+			}
+			else if constexpr (std::is_same_v<T, uint64_t>)
+			{
+				if (!v.is_number_unsigned())
+					return false;
+			}
+			else if constexpr (std::is_same_v<T, int64_t>)
+			{
+				if (!v.is_number_integer())
+					return false;
+			}
+			else if constexpr (std::is_same_v<T, float>)
+			{
+				if (!v.is_number_float())
+					return false;
+			}
+			else if constexpr (std::is_same_v<T, nlohmann::json> || std::is_same_v<T, json>)
+			{
+				if (!v.is_object())
+					return false;
+			}
+			else
+			{
+				static_assert(false, "unsupported value type!");
+			}
 
 			out = v;
+			
 			return true;
 		}
 		static json& from_nlohmann_json(nlohmann::json& j)
